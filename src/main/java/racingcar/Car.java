6ea193daf325625d2c3c59d1ed2d1930416aba73
khj1 <dhepd8849@gmail.com>
2022-12-02 18:28:27 +0900

    feat: 입력 값에 따라 자동차를 움직이는 기능 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,10 +1,13 @@
 package racingcar;
 
 import racingcar.model.CarName;
+import racingcar.model.Position;
 
 public class Car {
+    private static final int MOVABLE_LOWER_BOUND = 4;
+
     private final CarName name;
-    private int position = 0;
+    private Position position = Position.init();
 
     private Car(String name) {
         this.name = CarName.from(name);
@@ -14,5 +17,17 @@ public class Car {
         return new Car(name);
     }
 
-    // 추가 기능 구현
+    public void move(int command) {
+        if (isMovable(command)) {
+            position.increase();
+        }
+    }
+
+    private static boolean isMovable(int command) {
+        return command >= MOVABLE_LOWER_BOUND;
+    }
+
+    public boolean isAt(Position expectedPosition) {
+        return position.equals(expectedPosition);
+    }
 }
